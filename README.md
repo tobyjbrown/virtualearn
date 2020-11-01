ddfd518981a6f91d9d4a16136c74c4777f337de1
Toby Brown <tobybrown128@gmail.com>
2020-11-01 13:09:35 +0000

    Add functionality to carousel next and prev btns, document testing in readme

--- README.md
+++ README.md
@@ -13,3 +13,16 @@ TODO: Add further styles to expanded navigation, add interactivity to nav links 
 Subtle styles added to navigation, active class styled plus hover effect with psuedo-element. JS used to create simple dropdown menu and animate the rotation of the arrow.
 
 TODO: Make carousel functional, add hover effects to other links.
+
+3. 01/11/2020
+
+Rebuilt carousel component from scratch. Carried out the following manual testing during development:
+
+### Test 1 - Using the 'next' and 'previous' carousel buttons to update the 'current-card' class in the DOM.
+
+| Component | Intended function | Works as intended? | Fix |
+| --------- | ----------------- | ------------------ | --- |
+| **Carousel Next Button** | Removes 'current-card' class from the element that has it and adds it to that element's next sibling. | Yes, but throws error when class is on last sibling | Remove button from DOM when there are no siblings left |
+| **Carousel Previous Button** | Removes 'current-card' class from the element that has it and adds it to that element's previous sibling. | Yes, but throws error when class is on last sibling | Remove button from DOM when there are no siblings left |
+
+!["The DOM being manipulated when the user clicks the next/previous carousel buttons."](assets/write-up/carousel-next-prev-btns-class-test.gif)
--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,64 @@ $dropdownMenu.addEventListener('mouseleave', function() {
     $dropdownArrow.classList.remove('dropdown-arrow-open');
     $dropdownArrow.classList.add('dropdown-arrow-closed');
 });
+
+// carousel component
+
+const $carousel = document.querySelector('.carousel');
+const $carouselTrackContainer = document.querySelector('.carousel-track-container')
+const $carouselTrack = document.querySelector('.carousel-track');
+const $carouselCards = Array.from($carouselTrack.children);
+const $carouselNextBtn = document.querySelector('.carousel-button-next');
+const $carouselPrevBtn = document.querySelector('.carousel-button-prev');
+const $carouselNav = document.querySelector('.carousel-nav')
+const $carouselNavBtns = Array.from($carouselNav.children);
+
+let $currentCard;
+let $targetCard;
+
+const setCurrentCard = function() {
+    $currentCard = $carousel.querySelector('.current-card');
+}
+
+const updateCarouselButtons = () => {
+    if(!$currentCard.nextElementSibling) {
+        $carouselNextBtn.style.display = 'none';
+    } else if (!$currentCard.previousElementSibling){
+        $carouselPrevBtn.style.display = 'none';
+    } else {
+        $carouselNextBtn.style.display = 'block';
+        $carouselPrevBtn.style.display = 'block';
+    }
+}
+
+setCurrentCard();
+updateCarouselButtons();
+
+
+// find the width of the cards - each card will have same width
+// position the cards next to each other
+// move to next card when 'next' button is pressed
+// move to previous card when 'previous' button is pressed
+
+let cardWidth = $carouselCards[0].getBoundingClientRect().width;
+$carouselCards[0].style.left = 0 + 'px';
+$carouselCards[1].style.left = cardWidth + 'px';
+$carouselCards[2].style.left = cardWidth * 2 + 'px';
+
+$carousel.addEventListener('click', function(e) {
+    if(e.target.closest('button')) {
+        let btnClicked = e.target.closest('button');
+        setCurrentCard();
+        if(btnClicked.classList.contains('carousel-button-next')) {
+            $currentCard.classList.remove('current-card');
+            $targetCard = $currentCard.nextElementSibling;
+            $targetCard.classList.add('current-card');
+        } else if (btnClicked.classList.contains('carousel-button-prev')) {
+            $currentCard.classList.remove('current-card');
+            $targetCard = $currentCard.previousElementSibling;
+            $targetCard.classList.add('current-card');
+        }
+        setCurrentCard();
+        updateCarouselButtons();
+    }
+});
